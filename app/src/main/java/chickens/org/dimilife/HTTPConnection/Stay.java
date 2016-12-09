919bb3559668b381f117165253d2d4bb94bb7fd8
HaJunHyeok <pawn@boxbreakers.co>
2016-12-09 19:03:34 +0900

    StayService Finish

--- app/src/main/java/chickens/org/dimilife/HTTPConnection/Stay.java
+++ app/src/main/java/chickens/org/dimilife/HTTPConnection/Stay.java
@@ -5,4 +5,186 @@ package chickens.org.dimilife.HTTPConnection;
  */
 
 public class Stay {
+    private int stay_id;
+    private String stay_start_date;
+    private String stay_date;
+    private int user_id;
+    private String username;
+    private int grade;
+    private int Class;
+    private int number;
+    private String name;
+    private String apply_seat;
+    private int apply_breakfast;
+    private int apply_lunch;
+    private int apply_dinner;
+    private int apply_snack;
+    private int outgoing;
+    private String outgoing_start;
+    private String outgoing_end;
+
+    public int getStay_id() {
+        return stay_id;
+    }
+
+    public void setStay_id(int stay_id) {
+        this.stay_id = stay_id;
+    }
+
+    public String getStay_start_date() {
+        return stay_start_date;
+    }
+
+    public void setStay_start_date(String stay_start_date) {
+        this.stay_start_date = stay_start_date;
+    }
+
+    public String getStay_date() {
+        return stay_date;
+    }
+
+    public void setStay_date(String stay_date) {
+        this.stay_date = stay_date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getclass() {
+        return Class;
+    }
+
+    public void setClass(int Class) {
+        Class = this.Class;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApply_seat() {
+        return apply_seat;
+    }
+
+    public void setApply_seat(String apply_seat) {
+        this.apply_seat = apply_seat;
+    }
+
+    public int getApply_breakfast() {
+        return apply_breakfast;
+    }
+
+    public void setApply_breakfast(int apply_breakfast) {
+        this.apply_breakfast = apply_breakfast;
+    }
+
+    public int getApply_lunch() {
+        return apply_lunch;
+    }
+
+    public void setApply_lunch(int apply_lunch) {
+        this.apply_lunch = apply_lunch;
+    }
+
+    public int getApply_dinner() {
+        return apply_dinner;
+    }
+
+    public void setApply_dinner(int apply_dinner) {
+        this.apply_dinner = apply_dinner;
+    }
+
+    public int getApply_snack() {
+        return apply_snack;
+    }
+
+    public void setApply_snack(int apply_snack) {
+        this.apply_snack = apply_snack;
+    }
+
+    public int getOutgoing() {
+        return outgoing;
+    }
+
+    public void setOutgoing(int outgoing) {
+        this.outgoing = outgoing;
+    }
+
+    public String getOutgoing_start() {
+        return outgoing_start;
+    }
+
+    public void setOutgoing_start(String outgoing_start) {
+        this.outgoing_start = outgoing_start;
+    }
+
+    public String getOutgoing_end() {
+        return outgoing_end;
+    }
+
+    public void setOutgoing_end(String outgoing_end) {
+        this.outgoing_end = outgoing_end;
+    }
+
+    public String getOutgoing_cause() {
+        return outgoing_cause;
+    }
+
+    public void setOutgoing_cause(String outgoing_cause) {
+        this.outgoing_cause = outgoing_cause;
+    }
+
+    public int getApply_accommodation() {
+        return apply_accommodation;
+    }
+
+    public void setApply_accommodation(int apply_accommodation) {
+        this.apply_accommodation = apply_accommodation;
+    }
+
+    public String getEtc() {
+        return etc;
+    }
+
+    public void setEtc(String etc) {
+        this.etc = etc;
+    }
+
+    private String outgoing_cause;
+    private int apply_accommodation;
+    private String etc;
+
 }
--- app/src/main/java/chickens/org/dimilife/LoginActivity.java
+++ app/src/main/java/chickens/org/dimilife/LoginActivity.java
@@ -6,9 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import chickens.org.dimilife.HTTPConnection.LoginService;
 import chickens.org.dimilife.HTTPConnection.ServiceGenerator;
+import chickens.org.dimilife.HTTPConnection.Stay;
+import chickens.org.dimilife.HTTPConnection.StayService;
 import chickens.org.dimilife.HTTPConnection.User;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText id,pw;
     private Button button;
+    private TextView textView;
+    private String name;
+    private ArrayList<String> stayList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         id = (EditText)findViewById(R.id.editText);
         pw = (EditText)findViewById(R.id.editText2);
         button = (Button)findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,12 +55,37 @@ public class LoginActivity extends AppCompatActivity {
         call.enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                Log.e("id",response.body().getEmail());
+                name = response.body().getName();
+                getStay("2016-06-18");
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
                 t.printStackTrace();
+            }
+        });
+    }
+
+    private void getStay(String date) {
+        StayService stayService = ServiceGenerator.createService(StayService.class,"enoxaiming","hkwoo3");
+        Call<List<Stay>> call = stayService.getStay(date);
+        call.enqueue(new Callback<List<Stay>>() {
+            @Override
+            public void onResponse(Call<List<Stay>> call, Response<List<Stay>> response) {
+                for(int i = 0; i < response.body().size(); i++) {
+                    stayList.add(response.body().get(i).getName());
+                }
+                if(stayList.contains(name)) {
+                    textView.setText("너 잔류");
+                }
+                else {
+                    textView.setText("너 잔류 아니양");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Stay>> call, Throwable t) {
+
             }
         });
     }
