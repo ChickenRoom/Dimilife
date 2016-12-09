4e146031f58e4e97a4379bcb31b1bbf32edc3885
hyemdooly <hyemdooly@gmail.com>
2016-12-09 20:46:33 +0900

    asdf

--- app/src/main/java/chickens/org/dimilife/front/PagerAdapter.java
+++ app/src/main/java/chickens/org/dimilife/front/PagerAdapter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.LayoutInflater;
+
+import java.util.List;
 
 /**
  * Created by songhyemin on 2016. 12. 9..
@@ -12,15 +15,21 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
     Context context;
+    List<Fragment> listFragments;
+    LayoutInflater inflater;
 
 
     public PagerAdapter(FragmentManager fm) {
         super(fm);
+        listFragments.add();
+        listFragments.add();
+        listFragments.add();
+
     }
 
     @Override
     public Fragment getItem(int position) {
-        return null;
+        return Fragment.;
     }
 
     @Override
